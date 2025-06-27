4254ceef705a2cb6df0fef15677c302cde539f10
sam <contact@justsam.io>
2025-06-27 21:49:22 +0200

    Remove network-based API

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -18,8 +18,3 @@ def test_async_step_workflow_example(capsys):
     assert "async world" in out
 
 
-def test_network_workflow_example(capsys):
-    runpy.run_path(str(ROOT / "examples" / "network_workflow.py"), run_name="__main__")
-    out = capsys.readouterr().out
-    assert "Output:" in out
-    assert "7" in out
